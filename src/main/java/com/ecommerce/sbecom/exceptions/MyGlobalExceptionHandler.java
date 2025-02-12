1d982c6916025903b5a5d4e7969b3cb17aa1b6a9
ELEOMAR <kaotikus27@gmail.com>
2025-02-13 07:39:47 +0800

    added API response

--- src/main/java/com/ecommerce/sbecom/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/sbecom/exceptions/MyGlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ecommerce.sbecom.exceptions;
 
 
+import com.ecommerce.sbecom.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -28,15 +29,17 @@ public class MyGlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFound.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFound e){
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFound e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException e){
+    public ResponseEntity<APIResponse> myAPIException(APIException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ src/main/java/com/ecommerce/sbecom/payload/APIResponse.java
@@ -0,0 +1,17 @@
+package com.ecommerce.sbecom.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class APIResponse {
+
+    private String message;
+    private boolean status;
+
+}
+
