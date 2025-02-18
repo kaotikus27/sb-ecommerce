d084712c33c857d19f68c4f73589ec74efc33209
ELEOMAR <kaotikus27@gmail.com>
2025-02-18 08:01:14 +0800

    implementing product

---
+++ src/main/java/com/ecommerce/sbecom/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.ecommerce.sbecom.service;
+
+
+
+public interface ProductService {
+
+
+}
---
+++ src/main/java/com/ecommerce/sbecom/service/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ecommerce.sbecom.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+}
