e419b224fb0721fa0ebf774c28dbede1cdcc7c23
ELEOMAR <kaotikus27@gmail.com>
2025-03-04 13:21:59 +0800

    signup

--- src/main/java/com/ecommerce/sbecom/controller/AuthController.java
+++ src/main/java/com/ecommerce/sbecom/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.ecommerce.sbecom.controller;
 
+import com.ecommerce.sbecom.model.AppRole;
+import com.ecommerce.sbecom.model.Role;
+import com.ecommerce.sbecom.model.User;
+import com.ecommerce.sbecom.repositories.UserRepository;
 import com.ecommerce.sbecom.security.request.LoginRequest;
 import com.ecommerce.sbecom.security.response.UserInfoResponse;
 import com.ecommerce.sbecom.security.services.UserDetailsImpl;
 import com.ecommerce.sbecom.sercurity.jwt.JwtUtils;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -32,6 +36,8 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
@@ -64,6 +70,69 @@ public class AuthController {
                 jwtToken);
 
         return ResponseEntity.ok(response);
+        }
+
+
+        @PostMapping("/signup")
+        public ResponseEntity<?> registerUser(
+                @Valid @RequestBody SignUpRequest signUpRequest
+        ){
+            if(userRepository.existsByUsername(signUpRequest.getUsername())){
+                return ResponseEntity.badRequest().body(new MessageResponse(
+                        "Error: Username is already taken!"
+                ));
+            }
+            if(userRepository.existsByEmail(signUpRequest.getEmail())){
+                return ResponseEntity.badRequest().body(new MessageResponse(
+                        "Error: Email is already in use!"
+                ));
+            }
+
+            User user = new User(signUpRequest.getUsername(),
+                                 signUpRequest.getEmail(),
+                                 encoder.encode(signUpRequest.getPassword()));
+
+            Set<String> strRoles = signUpRequest.getRole();
+            Set<Role> roles = new HashSet<>();
+
+            if( strRoles == null){
+                Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                        .orEleseThrow(()-> new RuntimeException(
+                                "Error: Role is not found."
+                        ));
+                roles.add(userRole);
+            }else{
+                strRoles.forEach(role ->{
+                   switch (role){
+                       case "admin":
+                           Role adminrole = roleRepository.findyByRoleName(AppRole.ROLE_ADMIN)
+                                   .orElseThrow(()-> new RuntimeException(
+                                           "Error: Role in not found."
+                                   ));
+                           roles.add(adminrole);
+                           break;
+                       case "seller":
+                           Role modRole = roleRepository.findByRoleName(AppRole.ROLE_SELLER)
+                                   .orElseThrow(()-> new RuntimeException(
+                                           "Error: Role is not found."
+                                   ));
+                           roles.add(modRole);
+                           break;
+                       default:
+                           Role userRole = roleRepository.findbyRoleName(AppRole.ROLE_USER)
+                                   .orElseThrow(()-> new RuntimeException(
+                                           "Error: Role is not found"
+                                   ));
+                           roles.add(userRole);
+
+                   }
+                });
+            }
+
+            user.setRoles(roles);
+            userRepository.save(user);
+            return ResponseEntity.ok(new MessageResponse("User registered succesfully!"));
+
         }
 
 
--- src/main/java/com/ecommerce/sbecom/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/sbecom/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.ecommerce.sbecom.repositories;
 
 import com.ecommerce.sbecom.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional <User> findByUsername(String username);
+
+    boolean existsByUsername(@NotBlank @Size(min = 3, max = 50) String username);
+
 }
